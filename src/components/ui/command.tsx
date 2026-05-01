0d4ccb8bed48b8c3c94a49748c2a4855c7249fd3
Ronal9y <142257709+Ronal9y@users.noreply.github.com>
2026-05-01 17:55:37 -0400

    Projects

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,3 +1,4 @@
+// src/components/ui/command.tsx
 "use client"
 
 import * as React from "react"
@@ -59,7 +60,10 @@ function CommandDialog({
         )}
         showCloseButton={showCloseButton}
       >
-        {children}
+        {/* Aquí está el cambio: Envolver children en Command */}
+        <Command className="[&_[cmdk-group-heading]]:px-2 [&_[cmdk-group-heading]]:font-medium [&_[cmdk-group-heading]]:text-muted-foreground [&_[cmdk-group]:not([hidden])_~[cmdk-group]]:pt-0 [&_[cmdk-group]]:px-2 [&_[cmdk-input-wrapper]_svg]:h-5 [&_[cmdk-input-wrapper]_svg]:w-5 [&_[cmdk-input]]:h-12 [&_[cmdk-item]]:px-2 [&_[cmdk-item]]:py-3 [&_[cmdk-item]_svg]:h-5 [&_[cmdk-item]_svg]:w-5">
+          {children}
+        </Command>
       </DialogContent>
     </Dialog>
   )
